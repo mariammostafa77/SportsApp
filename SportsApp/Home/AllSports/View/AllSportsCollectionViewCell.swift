ba2a18e298e0f7c7bf396ef5bcc7061c1737cfbc
Asmaa <asmaayoussef786@gmail.com>
2022-05-11 17:07:06 +0200

    Add All Sport Tab to storyboard

---
+++ SportsApp/Home/AllSports/View/AllSportsCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AllSportsCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by user189298 on 5/11/22.
+//  Copyright © 2022 mariam mostafa. All rights reserved.
+//
+
+import UIKit
+
+class AllSportsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sportImage: UIImageView!
+    @IBOutlet weak var sportNameLabel: UILabel!
+}
