e92f09066e7ff14ba1efc0763b903e84b4f2a5f7
Asmaa <asmaayoussef786@gmail.com>
2022-05-15 21:03:43 +0200

    ......

---
+++ SportsApp/Module/Favorites/Presenter/FavoritePresenter.swift
@@ -0,0 +1,25 @@
+//
+//  FavoritePresenter.swift
+//  SportsApp
+//
+//  Created by user189298 on 5/15/22.
+//  Copyright © 2022 mariam mostafa. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class FavoritePresenter{
+    
+    var favCoreData: CoreDataService!
+    
+    func fetchFavoriteLeagues(appDelegate : AppDelegate) -> Array<NSManagedObject> {
+        favCoreData = CoreDataService(appDelegate : appDelegate)
+        return favCoreData.fetchLegueData()
+       }
+
+    func deleteOneLeagueFromFav(appDelegate : AppDelegate,leage:NSManagedObject) {
+        favCoreData = CoreDataService(appDelegate : appDelegate)
+    }
+    
+}
